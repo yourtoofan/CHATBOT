04eb2016a947f0be072648fdde1295c862e53ac1
Legend mickey <toofanpapa424@gmail.com>
2024-11-07 00:50:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "21803165"))
 API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
+OWNER_ID = int(getenv("OWNER_ID", "7520092354"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "WORLD_ALPHA")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "zoyu_support")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "The_incricible")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Legend_mickey")
 
 # Random Start Images
 IMG = [
